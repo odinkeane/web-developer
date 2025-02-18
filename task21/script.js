4953b006d251b151f41b7117ba2e32c5f46e8c35
odinkeane <138715688+odinkeane@users.noreply.github.com>
2025-02-18 13:22:08 +0700

    Add files via upload

---
+++ task21/script.js
@@ -0,0 +1,25 @@
+aside = document.querySelector('aside');
+main = document.querySelector('main');
+markdown = window.markdownit();
+
+let data;
+
+function setCurrent(i) {
+    main.innerHTML = markdown.render(data[i]);
+    for (let j = 0; j < data.length; j++) {
+        aside.children[j].style.background = ""
+        aside.children[j].style.color = ""
+    }
+    aside.children[i].style.background = "#826A5F";
+    aside.children[i].style.color = "#FFF";
+}
+
+window.addEventListener('load', async () => {
+    data = await fetch("task.json")
+        .then(response => response.json())
+        .then(data => data.data)
+    for (let i = 0; i < data.length; i++) {
+        aside.innerHTML += `<button onClick='setCurrent(${i})'>Задача №${i + 1}</button>`
+    }
+    setCurrent(0)
+})
